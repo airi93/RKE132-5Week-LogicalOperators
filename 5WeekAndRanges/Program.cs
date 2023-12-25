63a8c27692121e83f8df5029685490dc5f95573d
Irina Belik. <ibelik@tktk.ee>
2023-12-25 17:10:11 +0200

    add 5week code

---
+++ 5WeekAndRanges/Program.cs
@@ -0,0 +1,34 @@
+﻿// Kui õhutemp alla 0 - liiga külm
+//kui õhutemp üle 0 - külm
+//kui õhutemp 10-15 - jahe
+//kui õhutemp 16-20 -  soe
+//kui õhutemp 20-30 - palav
+//kui õhutemp 30 - väga palav
+
+Console.WriteLine("Enter the temperature:");
+int temp = Int32.Parse(Console.ReadLine());
+
+if (temp >= 30)
+{
+    Console.WriteLine("Boiling hot.");
+}
+else if (temp < 30 && temp >= 15)
+{
+    Console.WriteLine("Hot.");
+}
+else if (temp < 20 && temp >= 15)
+{
+    Console.WriteLine("Warm.");
+}
+else if (temp < 15 && temp >= 10)
+{
+    Console.WriteLine("Chilly.");
+}
+else if (temp < 10 && temp >= 0)
+{
+    Console.WriteLine("Cold.");
+}
+else
+{
+    Console.WriteLine("Freezing cold.");
+}
---
+++ 5WeekLogicalOperators/Program.cs
@@ -0,0 +1,11 @@
+﻿// And - &&
+//true && true ---> true
+//true && flase ---> false
+//false && true ---> false
+//false && false ---> false
+
+//OR - || (pipes) 
+//true || true ---> true
+//true || flase ---> true
+//false || true ---> true
+//false || false ---> false
